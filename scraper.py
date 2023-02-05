efde9d1539e6dfb8e88941279034f61561809a67
Accountant-28 <Carlsberg-28@yandex.ru>
2023-02-05 14:31:43 +0300

    Fixing

--- scraper.py
+++ scraper.py
@@ -1,86 +1,101 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup
 
 import requests
-
 import json
-import os
-
 import uuid
 
-PATH = '/Users/vladislav/PycharmProjects/WEBSCRAPING/trucks/data'
+PATH_FOR_SAVE = '/Users/vladislav/PycharmProjects/WEBSCRAPING/trucks/data'
 
 geckodriver = '/Users/vladislav/PycharmProjects/WEBSCRAPING/07_Sneakers/geckodriver'
 driver = webdriver.Firefox(executable_path=geckodriver)
 
 driver.get('https://www.truckscout24.de/transporter/gebraucht/kuehl-iso-frischdienst/renault')
 
+# Get rid of cookie
 cookie = driver.find_element(By.XPATH, '//*[@id="consent-mgmt-accept-all"]')
 cookie.click()
 
 soup = BeautifulSoup(driver.page_source, 'lxml')
 
+# Get all links for other pages
 row_pagination_links = [item.a for item in soup.find('ul', class_ = 'sc-pagination').find_all(['li'])][2:-1]
 pagination_links = [item['href'] for item in row_pagination_links]
 
+# Main while-loop for parse
 while True:
+    # Get each first car's link
     short_link = soup.find('a', {'data-item-name': 'detail-page-link'})['href']
     full_link = 'https://www.truckscout24.de/' + short_link
 
     car_link = requests.get(full_link)
     soup = BeautifulSoup(car_link.text, 'lxml')
 
+    # Get info from each car's link
     try:
         _id = int(uuid.uuid4())
         href = full_link
         title = soup.find('h1', class_ = 'sc-ellipsis sc-font-xl').text
+
         row_price = soup.find('h2', class_ = 'sc-highlighter-4 sc-highlighter-xl sc-font-bold').text
         price = int(row_price.replace('€ ', '').replace('.', '').replace(',-', ''))
+
         for item in soup.find('div', class_ = 'data-basic1').find_all('div', class_ = 'itemlbl'):
             if item.text == 'Kilometer':
                 row_mileage = item.find_next_sibling('div').text
                 mileage = int(row_mileage.replace('.', '').replace(' km', ''))
+
         for item in soup.find('ul', class_ = 'columns').find_all('div', class_ = 'sc-font-bold'):
             if item.text == 'Farbe':
                 color = item.find_next_sibling('div').text
+
         for item in soup.find('ul', class_ = 'columns').find_all('div', class_ = 'sc-font-bold'):
             if item.text == 'Leistung':
                 power = int(item.find_next_sibling('div').text[:3])
-        description = soup.find('div', {'data-type': 'description'}).find('p').text.replace('\r\n', '').replace('\u00A0', ' ')
+
+        row_description = soup.find('div', {'data-type': 'description'}).find('p').text
+        description = row_description.replace('\r\n', '').replace('\u00A0', ' ')
     except:
         pass
 
-    row_image = [item.img for item in soup.find('div', class_ = 'as24-carousel__container').find_all('div', class_ = 'as24-carousel__item')]
-    image_links = [item['data-src'] for item in row_image[:3]]
+    # Get links for the first three images of each car
+    row_images = [item.img for item in soup.find('div', class_ = 'as24-carousel__container').find_all('div', class_ = 'as24-carousel__item')]
+    image_links = [item['data-src'] for item in row_images[:3]]
 
-    os.makedirs(f'{PATH}/{_id}', exist_ok=True)
-    for image in image_links:
-        response = requests.get(image)
-        imagename = f'{PATH}/{_id}' + '/' + f'{uuid.uuid4()}.jpg'
-        with open(imagename, 'wb') as file:
-            file.write(response.content)
+    # Create new directory for each car and save images
+    # os.makedirs(f'{PATH_FOR_SAVE}/{_id}', exist_ok=True)
+    # for image in image_links:
+    #     response = requests.get(image)
+    #     imagename = f'{PATH_FOR_SAVE}/{_id}' + '/' + f'{uuid.uuid4()}.jpg'
+    #     with open(imagename, 'wb') as file:
+    #         file.write(response.content)
 
+    # Write necessary data for each car in file
     data = {
-        "ads": [
+        'ads': [
             {
-                "id": _id,
-                "href": href,
-                "title":title,
-                "price": price,
-                "mileage": mileage,
-                "color": color,
-                "power": power,
-                "description": description,
+                'id': _id,
+                'href': href,
+                'title': title,
+                'price': price,
+                'mileage': mileage,
+                'color': color,
+                'power': power,
+                'description': description,
             },
         ]
     }
 
-    with open(f'{PATH}/data.json', 'a', encoding='utf-8') as f:
+    with open(f'{PATH_FOR_SAVE}/data.json', 'a', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
         f.write(',')
+        f.write('\n')
 
+    # Pagenate through each page
     for link in pagination_links:
         next_page = requests.get(link)
     try:
